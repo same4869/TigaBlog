8b1de3a2dfaa5b47349dcb3dafc157bd0f28ad4e
王迅 <wangxun@hujiang.com>
2018-11-25 21:53:38 +0800

    log

--- src/test/java/com/xun/tigablog/TigablogApplicationTests.java
+++ src/test/java/com/xun/tigablog/TigablogApplicationTests.java
@@ -2,6 +2,8 @@ package com.xun.tigablog;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,6 +20,8 @@ public class TigablogApplicationTests {
     @Autowired
     DataSource dataSource;
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @Test
     public void contextLoads() throws SQLException {
         System.out.print(dataSource.getClass());
@@ -25,6 +29,16 @@ public class TigablogApplicationTests {
         Connection connection = dataSource.getConnection();
         System.out.print(connection);
         connection.close();
+    }
+
+    @Test
+    public void logTest(){
+        logger.trace("trace log");
+        logger.debug("debug log");
+        logger.info("info log");
+        logger.warn("warn log");
+        logger.error("error log");
+
     }
 
 }
